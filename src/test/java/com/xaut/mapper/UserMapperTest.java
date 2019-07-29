46c114c3796c4bc6af27e9de54b556c93100c041
wuxiaohua <1612571478@qq.com>
2019-07-29 20:49:15 +0800

    去除警告

--- src/test/java/com/xaut/mapper/UserMapperTest.java
+++ src/test/java/com/xaut/mapper/UserMapperTest.java
@@ -2,13 +2,9 @@ package com.xaut.mapper;
 
 import java.util.Date;
 import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.xaut.entity.User;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
