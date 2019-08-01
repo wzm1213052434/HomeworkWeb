d83a812510b494106e0eb84edd9844a8339764af
wuxiaohua <1612571478@qq.com>
2019-08-01 21:33:45 +0800

    发布作业

--- src/main/java/com/xaut/controller/WorkController.java
+++ src/main/java/com/xaut/controller/WorkController.java
@@ -1,12 +1,15 @@
 package com.xaut.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import com.xaut.entity.Work;
 import com.xaut.service.WorkService;
 
 @Controller
@@ -22,8 +25,31 @@ public class WorkController {
 	 */
 	@RequestMapping(value = "/publishWork",method=RequestMethod.GET)
 	public Map<String, Object> publishWork(HttpServletRequest request) {
-		//获取参数
-		//调用Service完成功能
-		return workService.publishWork();
+		//1.前端获取参数
+		String start_time = request.getParameter("beginDate").toString();
+		String end_time = request.getParameter("deadLine").toString();
+		String wName = request.getParameter("workName").toString();
+		String description = request.getParameter("content").toString();
+		String courseName_courseCno = request.getParameter("itemText").toString();
+		//String courseName = courseName_courseCno.split(",")[0];
+		String courseCno = courseName_courseCno.split(",")[1];
+		
+		//2.得到work对象
+		Work work = new Work();
+		work.setWno(courseCno + "");	//作业号 = 课程号 + 序号
+		work.setCno(courseCno);	//课程号
+		work.setwName(wName);	//作业名
+		work.setDesc(description);	//作业描述
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			work.setStart_time(sdf.parse(start_time));	//开始时间
+			work.setEnd_time(sdf.parse(end_time));	//结束时间
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		work.setUpdateTime(new java.sql.Date(new Date().getTime()));	//更新时间
+		
+		//3.调用Service完成功能
+		return workService.publishWork(work);
 	}
 }
--- src/main/java/com/xaut/service/WorkService.java
+++ src/main/java/com/xaut/service/WorkService.java
@@ -1,10 +1,11 @@
 package com.xaut.service;
 
 import java.util.Map;
+import com.xaut.entity.Work;
 
 public interface WorkService {
 	/**
 	 * 教师发布作业
 	 */
-	public Map<String, Object> publishWork();
+	public Map<String, Object> publishWork(Work work);
 }
